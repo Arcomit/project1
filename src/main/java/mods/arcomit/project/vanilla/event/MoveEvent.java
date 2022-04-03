bc0694ac4e777a8d617488f6cdc84c7730060fea
Moflop <8089341@qq.com>
2022-04-03 14:18:27 +0800

    自定义盔甲

---
+++ src/main/java/mods/arcomit/project/vanilla/event/MoveEvent.java
@@ -0,0 +1,31 @@
+package mods.arcomit.project.vanilla.event;
+
+import mods.arcomit.project.Project1;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @Author Arcomit
+ * @Update 2022/04/03-Arcomit
+ * 玩家疾跑时可跨越一格方块
+ */
+@Mod.EventBusSubscriber(modid = Project1.MODID)
+public class MoveEvent {
+    @SubscribeEvent
+    public static void updatePlayerMaxUpStep(LivingEvent.LivingUpdateEvent event){
+        if (event.getEntityLiving() instanceof Player){
+            Player player = (Player) event.getEntityLiving();
+            if (player.isSprinting()){//判断是否疾跑
+                if(player.maxUpStep!=1.25F){//判断最大可跨越高度
+                    player.maxUpStep = 1.25F;
+                }
+            }else {
+                if(player.maxUpStep!=0.6F){
+                    player.maxUpStep = 0.6F;
+                }
+            }
+        }
+    }
+}
